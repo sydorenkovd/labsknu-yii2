8f84db54b17648a5607dc0cb6cbea8d274830a44
sydorenkovd <sydorenkovd@gmail.com>
2016-05-30 10:53:56 +0300

    test

--- frontend/controllers/SiteController.php
+++ frontend/controllers/SiteController.php
@@ -72,7 +72,9 @@ class SiteController extends Controller
      */
     public function actionIndex()
     {
-        return $this->render('index');
+        $this->layout = '';
+        $s = print_r(Yii::$app->db);
+        return $this->render('index', compact('s'));
     }
 
     /**
